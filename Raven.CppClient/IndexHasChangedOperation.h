aa7a306210831eb29adb6b2b6ee22cafa04f0de6
sashatim125 <alexander@ravendb.net>
2018-12-09 17:35:24 +0200

    IndexHasChangedOperation added

---
+++ Raven.CppClient/IndexHasChangedOperation.h
@@ -0,0 +1,77 @@
+#pragma once
+#include "stdafx.h"
+#include "IMaintenanceOperation.h"
+#include "RavenCommand.h"
+#include "ServerNode.h"
+#include "IndexDefinition.h"
+#include "json_utils.h"
+
+namespace ravendb::client::documents::operations::indexes
+{
+	using
+		ravendb::client::http::RavenCommand,
+		ravendb::client::http::ServerNode,
+		ravendb::client::documents::indexes::IndexDefinition;
+
+	class IndexHasChangedOperation : public IMaintenanceOperation<bool>
+	{
+	private:
+		const IndexDefinition _index_definition;
+
+	public:
+		~IndexHasChangedOperation() override = default;
+
+		IndexHasChangedOperation(IndexDefinition definition)
+			: _index_definition(std::move(definition))
+		{}
+
+		std::unique_ptr<RavenCommand<bool>> get_command(const DocumentConventions& conventions) override
+		{
+			return std::unique_ptr<RavenCommand<bool>> (
+				std::make_unique<IndexHasChangedCommand>(conventions, _index_definition));
+		}
+
+	private:
+		class IndexHasChangedCommand : public RavenCommand<bool>
+		{
+		private:
+			nlohmann::json _definition;
+
+		public:
+			~IndexHasChangedCommand() override = default;
+
+			IndexHasChangedCommand(const DocumentConventions& conventions, const IndexDefinition& definition)
+				: _definition(definition)
+			{}
+
+			void create_request(CURL* curl, const ServerNode& node, std::string& url) override
+			{
+				std::ostringstream pathBuilder;
+				pathBuilder << node.url << "/databases/" << node.database
+					<< "/indexes/has-changed";
+
+				curl_easy_setopt(curl, CURLOPT_HTTPPOST, 1);
+
+				auto&& json_str = _definition.dump();
+
+				curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, json_str.c_str());
+
+				url = pathBuilder.str();
+			}
+
+			void set_response(CURL* curl, const nlohmann::json& response, bool from_cache) override
+			{
+				if(! impl::utils::json_utils::get_val_from_json(response, "Changed", _result))
+				{
+					throw ravendb::client::RavenError({}, ravendb::client::RavenError::ErrorType::invalid_response);
+				}
+			}
+
+			bool is_read_request() const noexcept override
+			{
+				return false;
+			}
+		};
+	};
+}
+
