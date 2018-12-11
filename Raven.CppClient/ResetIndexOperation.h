0c77fff000f31ffab0361bd913dfa0627ac7abb5
sashatim125 <alexander@ravendb.net>
2018-12-11 15:31:09 +0200

    ResetIndexOperation added

---
+++ Raven.CppClient/ResetIndexOperation.h
@@ -0,0 +1,76 @@
+#pragma once
+#include "stdafx.h"
+#include "IVoidMaintenanceOperation.h"
+#include "utils.h"
+
+using
+ravendb::client::http::RavenCommand,
+ravendb::client::http::ServerNode,
+ravendb::client::http::VoidRavenCommand;
+
+namespace ravendb::client::documents::operations::indexes
+{
+	class ResetIndexOperation : public IVoidMaintenanceOperation
+	{
+	private:
+		const std::string _index_name;
+
+	public:
+		~ResetIndexOperation() override = default;
+
+		explicit ResetIndexOperation(std::string index_name)
+			: _index_name([&]
+		{
+			if (index_name.empty())
+			{
+				throw std::invalid_argument("Index name must have a non empty value");
+			}
+			else
+			{
+				return std::move(index_name);
+			}
+		}())
+		{}
+
+		std::unique_ptr<VoidRavenCommandBase> get_command(const DocumentConventions& conventions) override
+		{
+			return std::make_unique<ResetIndexCommand>(_index_name);
+		}
+
+	private:
+		class ResetIndexCommand : public VoidRavenCommand
+		{
+		private:
+			const std::string _index_name;
+
+		public:
+			~ResetIndexCommand() override = default;
+
+			explicit ResetIndexCommand(std::string index_name)
+				: _index_name([&]
+			{
+				if (index_name.empty())
+				{
+					throw std::invalid_argument("Index name must have a non empty value");
+				}
+				else
+				{
+					return std::move(index_name);
+				}
+			}())
+			{}
+
+			void create_request(CURL* curl, const ServerNode& node, std::string& url) override
+			{
+				std::ostringstream pathBuilder;
+				pathBuilder << node.url << "/databases/" << node.database
+					<< "/indexes?name=" << impl::utils::url_escape(curl, _index_name);
+
+				curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "RESET");
+
+				url = pathBuilder.str();
+			}
+		};
+	};
+}
+
