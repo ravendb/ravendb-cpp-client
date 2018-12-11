63897f9c23df64c3277d1b53c61d23c81a644f72
sashatim125 <alexander@ravendb.net>
2018-12-11 16:08:42 +0200

    StartIndexingOperation and StopIndexingOperation added

---
+++ Raven.CppClient/StopIndexingOperation.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "stdafx.h"
+#include "IVoidMaintenanceOperation.h"
+
+using
+ravendb::client::http::RavenCommand,
+ravendb::client::http::ServerNode,
+ravendb::client::http::VoidRavenCommand;
+
+namespace ravendb::client::documents::operations::indexes
+{
+	class StopIndexingOperation : public IVoidMaintenanceOperation
+	{
+	public:
+		~StopIndexingOperation() override = default;
+
+		StopIndexingOperation() = default;
+
+		std::unique_ptr<VoidRavenCommandBase> get_command(const DocumentConventions& conventions) override
+		{
+			return std::make_unique<StopIndexingCommand>();
+		}
+
+	private:
+		class StopIndexingCommand : public VoidRavenCommand
+		{
+		public:
+			~StopIndexingCommand() override = default;
+
+			StopIndexingCommand() = default;
+
+			void create_request(CURL* curl, const ServerNode& node, std::string& url) override
+			{
+				std::ostringstream pathBuilder;
+				pathBuilder << node.url << "/databases/" << node.database
+					<< "/admin/indexes/stop";
+
+				curl_easy_setopt(curl, CURLOPT_HTTPPOST, 1);
+				curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, "");
+
+				url = pathBuilder.str();
+			}
+		};
+	};
+}
+
